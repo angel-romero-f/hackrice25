1481a11953a4d6e1b68f570d69ec03edad518eda
Noah Villa <noahvilla@mac.lan>
2025-09-20 13:48:17 -0500

    Removal

--- backend/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGODB_URI = os.getenv("mongodb+srv://ar155_db_user:fOFTDd0vnh0sMFwj@cluster0.axnlx9w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-GOOGLE_MAPS_API_KEY = os.getenv("AIzaSyBgHWrGOlDhpIC0qpVMbnrm4Xc9wRYipZE")
-GEMINI_API_KEY = os.getenv("AIzaSyAInImQl1tsVYvSUlfnWh-HdnNyGoiBqvo")
